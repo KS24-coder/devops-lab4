f58e484800a43cf96ec0dca98b5251a89d942244
Khushboo <2021wa86974@wipl.bits-pilani.ac.in>
2025-07-01 19:55:41 +0530

    Updated WelcomeMessage to confirm CI functionality

--- src/main/java/WelcomeMessage.java
+++ src/main/java/WelcomeMessage.java
@@ -1,5 +1,5 @@
 public class WelcomeMessage {
     public static void main(String[] args) {
-        System.out.println("Welcome to Bits Pilani,continous integration in Action");
+        System.out.println("Welcome to Bits Pilani,continous integration in Action,continous is workinf fine");
     }
 }
